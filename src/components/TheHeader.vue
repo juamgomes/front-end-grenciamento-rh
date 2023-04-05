00cfeb58c2eeb379ad67375c9930de3552da1eab
Joao Mateus <joao.amaral@ifam.edu.br>
2023-04-04 22:38:59 -0300

    fix: hover on navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -18,8 +18,11 @@
             <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <a v-for="(item, index) in navigation" :key="item.name" :href="item.href" 
-                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
-                :aria-current="item.current ? 'page' : undefined" @click="updateCurrent(index)">{{ item.name }}</a>
+                @click.prevent="updateCurrent(index)"
+                :class="{ 'text-red': item.current, 'text-white': !item.current }"
+                :aria-current="item.current ? 'page' : undefined">
+                  {{ item.name }}
+              </a>
             </div>
             </div>
           </div>
@@ -64,7 +67,7 @@
           <DisclosureButton 
             v-for="item in navigation" :key="item.name"
             as="a" :href="item.href" 
-            :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+            :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : null">{{ item.name }}</DisclosureButton>
         </div>
         <div class="border-t border-gray-700 pt-4 pb-3">
           <div class="flex items-center px-5">
@@ -105,8 +108,8 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-import Vue from 'vue';
-
+import { reactive } from 'vue';
+// import Vue from 'vue';
 
 const user = {
   name: 'Tom Cook',
@@ -115,26 +118,16 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 
-const navigation = [
-  { name: 'Inicio', href: '/home', current: true},
+const navigation = reactive([
+  { name: 'Inicio', href: '/home', current: false},
   { name: 'Funcionários', href: '/Funcionarios', current: false},
   { name: 'Setores', href: '/setor', current: false},
   { name: 'Empresa', href: '/Empresa', current: false },
-]
+])
 
-function updateCurrent(index) {
-  this.navigation.forEach((item, i) => {
-    if (i === index) {
-      Vue.set(item, 'current', true);
-    } else {
-      Vue.set(item, 'current', false);
-    }
-  });
+function updateCurrent(position) {
+  navigation.forEach((item, index) => {
+    item.current = position === index;
+  })
 }
-
-const userNavigation = [
-  { name: 'Seu perfil', href: '#' },
-  { name: 'Configurações', href: '#' },
-  { name: 'Sair', href: '#' },
-]
 </script>
